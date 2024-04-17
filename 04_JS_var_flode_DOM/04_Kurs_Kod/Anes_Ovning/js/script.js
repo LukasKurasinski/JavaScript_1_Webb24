cbbe2cb2aa41a7ab16c921a6968cb5aa992fbdd5
Anes075 <166699658+Anes075@users.noreply.github.com>
2024-04-17 11:18:43 +0200

    Update script.js

--- 04_JS_var_flode_DOM/04_Kurs_Kod/Anes_Ovning/js/script.js
+++ 04_JS_var_flode_DOM/04_Kurs_Kod/Anes_Ovning/js/script.js
@@ -1,8 +1,8 @@
 let cell = "";
 let sum;
 
-for(let i = 1; i < 6; i++){
-    for(let j = 1; j < 6; j++){
+for(let i = 1; i <= 5; i++){
+    for(let j = 1; j <= 5; j++){
 
         sum = i*j;
         cell = cell + "| " + sum + " |";
@@ -17,16 +17,21 @@ let counter = 0;
 
 let rem = 0;
 
-for(let i = 1; i < 11; i++){
-    for(let j = 1; j < 11; j++){
+for(let i = 1; i <= 10; i++){
+    for(let j = 1; j <= 10; j++){
 
         counter = counter + 1;
 
         rem = counter% 2;
 
         if(rem != 0){
-        cell = cell + " | " + counter + " | ";
+            if(counter < 10){
+        cell = cell + " | " + "0" + counter + " | ";
         }
+        else{
+            cell = cell + " | " + counter + " | ";
+        } 
+    }
     }
     console.log(cell);
     cell = "";
