dc817972932679c147318702f94d3263d713520f
Anes075 <166699658+Anes075@users.noreply.github.com>
2024-04-17 11:12:13 +0200

    Övning 2

--- 04_JS_var_flode_DOM/04_Kurs_Kod/Anes_Ovning/js/script.js
+++ 04_JS_var_flode_DOM/04_Kurs_Kod/Anes_Ovning/js/script.js
@@ -10,3 +10,24 @@ for(let i = 1; i < 6; i++){
     console.log(cell);
     cell = "";
 }
+
+console.log("\n \n");
+
+let counter = 0;
+
+let rem = 0;
+
+for(let i = 1; i < 11; i++){
+    for(let j = 1; j < 11; j++){
+
+        counter = counter + 1;
+
+        rem = counter% 2;
+
+        if(rem != 0){
+        cell = cell + " | " + counter + " | ";
+        }
+    }
+    console.log(cell);
+    cell = "";
+}
