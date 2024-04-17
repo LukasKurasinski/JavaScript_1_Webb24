1915da8729c3dc41b6cd3ace32e9f99ef77f6532
Anes075 <166699658+Anes075@users.noreply.github.com>
2024-04-17 11:24:07 +0200

    Senaste

--- 04_JS_var_flode_DOM/04_Kurs_Kod/Anes_Ovning/js/script.js
+++ 04_JS_var_flode_DOM/04_Kurs_Kod/Anes_Ovning/js/script.js
@@ -1,15 +1,29 @@
+console.log("Multiplikationstabell 5x5");
+
+console.log("\n \n");
+
+
+
 let cell = "";
 let sum;
 
 for(let i = 1; i <= 5; i++){
     for(let j = 1; j <= 5; j++){
-
-        sum = i*j;
+    sum = i*j;
+    if(sum < 10){
+        cell = cell + "| " + "0" + sum + " |"; 
+    }
+    else{
         cell = cell + "| " + sum + " |";
+    }
     }
     console.log(cell);
     cell = "";
 }
+
+console.log("\n \n");
+
+console.log("Udda tal mellan 1-100");
 
 console.log("\n \n");
 
