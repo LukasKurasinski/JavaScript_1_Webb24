715e97f9b99d5b1ba2c46452b2febb4088d9227d
Anes075 <166699658+Anes075@users.noreply.github.com>
2024-04-30 14:36:29 +0200

    09

--- 09_Objekt_Array_List/Anes_Ovning/js/script.js
+++ 09_Objekt_Array_List/Anes_Ovning/js/script.js
@@ -22,3 +22,68 @@ console.log('Last index has the value ' + lastIndex);
 myArray.push('10');
 console.log(myArray);
 console.log('My array length is',lengd = myArray.length);
+
+//pop()
+
+let deletedLastIndex = myArray.pop();
+console.log(myArray);
+console.log('Last element deleted ' + myArray);
+console.log('The deleted index value is ' + deletedLastIndex);
+
+let newValue = 100;
+myArray.push(newValue);
+console.log(myArray);
+
+//For loop with push() pop()
+
+console.log('');
+console.log('');
+
+for(let index = 0; index <= myArray.length-1; index++){
+    console.log('We are on index ' + index);
+    console.log('Value is: ' + myArray[index]);
+}
+
+//For loop that prints out values backwards;
+
+console.log('');
+console.log('');
+
+for(let index = myArray.length-1; index >= 0; index--){
+    console.log('We are on index ' + index);
+    console.log('Value is: ' + myArray[index]);
+}
+
+//0 ubdex and length-1 index is the same
+let array1 = ['B'];
+
+//Double for loop array
+
+
+function changeMatrixSize(){
+
+let slider = document.getElementById("slider")
+
+    
+let rows = slider.value;
+let cols = slider.value;
+let matrix = [];
+
+for(let i = 0; i < rows; i++){
+    let row = [];
+    for(let j = 0; j < cols; j++){
+        row.push(j);
+    }
+    matrix.push(row);
+}
+console.log(matrix);
+}
+
+//Splice()
+
+let listMiddleIndex = (myArray.length-1)/2;
+myArray.splice(listMiddleIndex, listMiddleIndex+1);
+console.log(myArray);
+
+//Slice()
+
