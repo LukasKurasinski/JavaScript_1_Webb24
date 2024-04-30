8368979647bba23329c460e5bc0c8753e06ec62a
Anes075 <166699658+Anes075@users.noreply.github.com>
2024-04-30 11:16:03 +0200

    09

---
+++ 09_Objekt_Array_List/Anes_Ovning/js/script.js
@@ -0,0 +1,24 @@
+//Array and lists
+
+let myArray = [1,2,3,4,5,6,7,8,9];
+console.log(myArray);
+console.log('My array length is ' + myArray.length);
+
+//Define length variable
+
+let lengd = myArray.length;
+console.log('My array length is ' + lengd);
+
+//Get value at index 0
+
+let firstIndex = myArray[0];
+console.log('First index has the value ' + firstIndex);
+
+let lastIndex = myArray[lengd-1];
+console.log('Last index has the value ' + lastIndex);
+
+//Push()
+
+myArray.push('10');
+console.log(myArray);
+console.log('My array length is',lengd = myArray.length);
