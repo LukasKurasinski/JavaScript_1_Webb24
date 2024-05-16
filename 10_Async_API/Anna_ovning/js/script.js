6ddc40c99ce945ad23cd625ae7a8fe4d6c73cfe5
AnnaKlingenstierna <149709837+AnnaKlingenstierna@users.noreply.github.com>
2024-05-16 09:33:21 +0200

    m

---
+++ 10_Async_API/Anna_ovning/js/script.js
@@ -0,0 +1,69 @@
+function writeName (x) {
+    console.log (x)
+}
+
+writeName("Anna");
+
+
+
+// beräknar arean
+let width = 10;
+let height = 20;
+
+function area () {
+return width * height}
+
+console.log (area ());
+
+
+
+// Returnera summan av ett tal
+function sum (a,b) {
+    return a + b
+}
+console.log (sum());
+
+
+
+//Räkna ut fahrenheit to celsius
+
+let celcius = (c)=> {
+    return (c * 9/5) + 32;
+}
+console.log (celcius(25));
+
+
+
+/* let langd = (l) => {
+    return (l.length)
+} 
+ 
+console.log (l ("Anna")); */
+
+
+
+
+// Få det till stora bokstäver
+let upperCase =  (u) => u.toUpperCase ();
+
+console.log (upperCase ("hej"));
+
+/* 
+let text = "text här".toUpperCase();
+let textrep = text.replace ("", "");
+let textrev = textReplace.concat
+console.log (textrep) */
+
+ let text2= "annan text".toUpperCase().replace(" ", " ").concat ("Ny text")
+ console.log (text2)
+
+
+
+
+
+ /* API, steg 1, hämta en api länk */
+
+/* https://http.cat/401 */
+
+
+
